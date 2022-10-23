bf9f2d9879bec79374dc641f80b6765004be43e3
Gwanghee11 <112791482+Gwanghee11@users.noreply.github.com>
2022-10-23 16:16:19 +0900

    Add files via upload

---
+++ html,css/js20_1.js
@@ -0,0 +1,9 @@
+function calc() {
+  const in1 =document.getElementById('n1');
+  const in2 =document.getElementById('n2');
+  const res =document.getElementById('result');
+  var n1 = parseInt(in1.value);
+  var n2 = parseInt(in2. value);
+  s = n1 + n2;
+  res.innerText = s;
+}
